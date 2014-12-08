513e24850e6c0db402530ea715eadbf0fb8076ff
Alan <alanng95@hotmail.com>
2014-12-09 09:21:40 +1100

    rake db:migrate last stuff

--- Jigglr/db/schema.rb
+++ Jigglr/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208212149) do
+ActiveRecord::Schema.define(version: 20141208221855) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
