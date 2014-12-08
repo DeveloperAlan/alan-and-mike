a353fcb105200f12c672b6f9c5564eb0e39131ea
Alan <alanng95@hotmail.com>
2014-12-08 18:18:12 +1100

    added devise user'

--- Jigglr/app/models/user.rb
+++ Jigglr/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :wishlists
-  has_one :search_list
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
