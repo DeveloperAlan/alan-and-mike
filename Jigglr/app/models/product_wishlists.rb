5f42b6fa2b6b887a23f3e777ea3f76bcad3eeb41
Alan <alanng95@hotmail.com>
2014-12-09 16:01:24 +1100

    mike's backup'

--- Jigglr/app/models/product_wishlists.rb
+++ Jigglr/app/models/product_wishlists.rb
@@ -1,2 +1,4 @@
 class ProductWishlists < ActiveRecord::Base
+  has_many :products 
+  has_many :wishlists
 end
---
+++ Jigglr/config/initializers/rebay.rb
@@ -0,0 +1,3 @@
+Rebay::Api.configure do |rebay|
+    rebay.app_id = 'AlanNg66c-9363-4c8d-a936-ecdbc45fb2b'
+end
